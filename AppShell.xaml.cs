ba8a6e189a6286ca366da342a51ae763b7350f35
zDany01 <85403430+zDany01@users.noreply.github.com>
2025-03-01 14:33:41 +0100

    GUI Structure

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SpotilightController
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GUI.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace SpotilightController
+{
+    public partial class GUI : ContentPage
+    {
+        const string BULB_OFF_STATUS_LABEL = "Off";
+        const string BULB_ON_STATUS_LABEL = "On";
+
+        public string BulbStatus { get; set; }
+
+        public GUI()
+        {
+            InitializeComponent();
+            this.bulbSwitch.PropertyChanged += SwitchBulb;
+            this.bulbLbl.Text = BULB_OFF_STATUS_LABEL;
+        }
+
+        private void SwitchBulb(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            bulbLbl.Text = bulbSwitch.IsToggled ? BULB_ON_STATUS_LABEL : BULB_OFF_STATUS_LABEL;
+        }
+    }
+
+}
